338fb10180441c7c8f87d973a1def3750447fa03
Bruno <brunosilvabrait@gmail.com>
2023-05-31 18:34:50 -0300

    Creating a Custom Validation - Part 1

--- config/application.rb
+++ config/application.rb
@@ -36,5 +36,8 @@ module EcommerceApi
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+
+    config.auto_load_paths += %w["#{config.root}/app/validators/"]
+
   end
 end
