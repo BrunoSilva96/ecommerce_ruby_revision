92c10a660e76fb1157a988ef09ca5b2494082448
Bruno <brunosilvabrait@gmail.com>
2023-06-07 16:50:55 -0300

    Removing a Category

--- app/controllers/admin/v1/categories_controller.rb
+++ app/controllers/admin/v1/categories_controller.rb
@@ -1,5 +1,6 @@
 module Admin::V1
   class CategoriesController < ApiController
+    before_action :load_category, only: [:update, :destroy]
 
     def index
       @categories = Category.all
@@ -12,9 +13,25 @@ module Admin::V1
     end
 
     def update
-      @category = Category.find(params[:id])
       @category.attributes = category_params
       save_category!
+    end
+
+    def destroy
+      @category.destroy!
+    rescue
+      render_error(fields: @category.errors.messages)
+    end
+
+    private
+
+    def load_category
+      @category = Category.find(params[:id])
+    end
+
+    def category_params
+      return {} unless params.has_key?(:category)
+      params.require(:category).permit(:id, :name)
     end
 
     def save_category!
@@ -22,13 +39,6 @@ module Admin::V1
       render :show
     rescue
       render_error(fields: @category.errors.messages)
-    end
-
-    private
-
-    def category_params
-      return {} unless params.has_key?(:category)
-      params.require(:category).permit(:id, :name)
     end
 
   end
